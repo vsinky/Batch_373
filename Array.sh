f42ed4a5e5df3bbc5196e7678610d1092686dd2b
vsinky <sinkykumariverma@gmail.com>
2021-09-23 19:21:02 +0530

    added file

---
+++ Array.sh
@@ -0,0 +1,6 @@
+int a=0;
+int array[] = new int [15];
+for ( a=1; a<=19; a++) 
+{
+    System.out.println(array[a])
+}
---
+++ Forloop.sh
@@ -0,0 +1,6 @@
+names='Stan Kyle Cartman'
+for name in $names
+do
+echo $name
+done
+echo All done
